1957cd10bacbf185f58c7f98658264b4a6f8f18e
Pxdeveloper1 <126421644+Pxdeveloper1@users.noreply.github.com>
2023-02-26 22:05:09 +0100

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,5 +1,4 @@
 --UNA ANTISILENT--
 Config = {}
-Config.License = "458-652-524-558"
-Config.Weebhook = "https://discord.com/api/webhooks/1056237026187427843/4V3DnaiNob9lzz5ErS62BWFMcCE71daVxi9SOSEJxuSiC3_pTaZNow9O5o7Po7mGQL0R"
+Config.Weebhook = "" --TU WEEBHOOK
 Config.Msg  = "SE TE HA DETECTADO SILENT AIM"
